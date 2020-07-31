8677a631324739ea4bb3e07ededcfad4a2cb264d
peranovicc <cvijanperanovic@aol.com>
2020-07-31 09:53:50 +0200

    Вежбање низова - петак

---
+++ 3. Недеља/nizoviVezbanje.js
@@ -0,0 +1,41 @@
+// Користећи петљу исписати све људе (for / forEach)
+{
+    let ljudi = ['Pera','Ana','Zika','Lana']
+
+}
+
+// Уклонити 'Pera' из низа
+{
+    let ljudi = ['Pera','Ana','Zika','Lana']
+
+}
+
+// Уклонити 'Lana' из низа
+{
+    let ljudi = ['Pera','Ana','Zika','Lana']
+
+}
+
+// Додати 'Mika' на почетак
+{
+    let ljudi = ['Pera','Ana','Zika','Lana']
+
+}
+
+// Додати 'Mika' на крај
+{
+    let ljudi = ['Pera','Ana','Zika','Lana']
+
+}
+
+// Исписати све људе, али изаћи из петље кад се стигне до 'Ana'
+{
+    let ljudi = ['Pera','Ana','Zika','Lana']
+
+}
+
+// Наћи 'Zika' у низу (тј његову позицију)
+{
+    let ljudi = ['Pera','Ana','Zika','Lana']
+
+}
