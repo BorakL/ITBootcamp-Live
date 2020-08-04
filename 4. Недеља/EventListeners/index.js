885d462631b7e3352e80e2a3ff15f7936ed2782a
peranovicc <cvijanperanovic@aol.com>
2020-08-04 11:14:39 +0200

    sort, event listeners

---
+++ 4. Недеља/EventListeners/index.js
@@ -0,0 +1,18 @@
+const dugme = document.getElementById('dugme')
+const count = document.getElementById('count')
+
+let i = 0
+
+dugme.addEventListener('click',(event) => {
+    console.log(event.target)
+})
+
+const forma = document.querySelector('#form')
+const inputUsername = document.querySelector('#username')
+const sb = document.querySelector('#select')
+
+forma.addEventListener('submit',(e) => {
+    e.preventDefault()
+    console.log(inputUsername.value)
+    console.log(sb.value)
+})
