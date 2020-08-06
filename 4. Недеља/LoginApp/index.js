e52da6cfeca22d1ac9b585bb6d43c572616c9987
peranovicc <cvijanperanovic@aol.com>
2020-08-06 09:45:26 +0200

    Fake login app - starter

---
+++ 4. Недеља/LoginApp/index.js
@@ -0,0 +1,31 @@
+const users = [
+    {
+        username: 'admin',
+        password: 'admin',
+        isAdmin: true
+    },
+    {
+        username: 'John',
+        password: '123456',
+        isAdmin: false,
+        coins: 7
+    },
+    {
+        username: 'Kelly',
+        password: 'asdf1234',
+        isAdmin: false,
+        coins: 12
+    },
+    {
+        username: 'Pera',
+        password: '1234asdf',
+        isAdmin: false,
+        coins: 42
+    },
+    {
+        username: 'Ana',
+        password: 'fghfhf',
+        isAdmin: false,
+        coins: 31
+    }
+]
