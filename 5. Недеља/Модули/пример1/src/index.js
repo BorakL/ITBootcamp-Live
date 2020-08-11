0f448d960c500463e12fd1196d920e69a3e7e8ec
peranovicc <cvijanperanovic@aol.com>
2020-08-11 14:04:37 +0200

    Модули - уторак

--- 5. Недеља/Модули/пример1/src/index.js
+++ 5. Недеља/Модули/пример1/src/index.js
@@ -1,3 +1,22 @@
-import { obimKvadrata } from './matematika.js'
+import M from './matematika.js'
 
-console.log(obimKvadrata(6))
+console.log(M)
+console.log(avg(3,10))
+console.log(M.obim(3,10))
+console.log(M.povrsina(3,10))
+console.log(M.brojevi)
+M.promeniX(548)
+console.log(M.x)
+
+// npm, yarn
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+
+    if(valid()){
+        dodajNaStranicu()
+    }
+    else{
+        ispisiGresku()
+    }
+})
--- 5. Недеља/Модули/пример1/src/matematika.js
+++ 5. Недеља/Модули/пример1/src/matematika.js
@@ -1,6 +1,55 @@
-export function obimKvadrata(a){
-    return 4 * a
+// export function obim(a,b = a){
+//     return 2*a + 2*b
+// }
+
+// export function povrsina(a,b = a){
+//     return a * b
+// }
+
+// export function avg(N,M){
+//     let suma = 0
+//     let count = 0
+//     for(let i = N; i < M; i++){
+//         suma += i
+//         count++
+//     }
+//     return suma / count
+// }
+
+// export default function mnozenje(){
+//     console.log('kjsfjksdf')
+// }
+
+
+
+function obim(a,b = a){
+    return 2*a + 2*b
 }
 
-// export { obimKvadrata }
+function povrsina(a,b = a){
+    return a * b
+}
 
+function avg(N,M){
+    let suma = 0
+    let count = 0
+    for(let i = N; i < M; i++){
+        suma += i
+        count++
+    }
+    return suma / count
+}
+
+const niz = [1,2,3,4,5,5]
+
+let x = 5
+
+const fkjdd = {
+    avg: avg,
+    obim: obim,
+    povrsina: povrsina,
+    brojevi: niz,
+    x: x,
+}
+
+export default fkjdd
