95412b4d78476de8bc9386c5af24d60980a3cc9b
peranovicc <cvijanperanovic@aol.com>
2020-08-25 10:41:19 +0200

    Launch component

---
+++ 6. Недеља/spacex/src/index.js
@@ -0,0 +1,23 @@
+const app = document.querySelector('#app')
+
+// Функција (Компонента) Launch враћа хтмл елемент
+const Launch = (launch) => {
+    // launch.launch_year
+    // launch.rocket.rocket_name
+    // launch.links.mission_patch  // slika
+    const launch = document.createElement('div')
+    launch.className = 'launch'
+
+}
+
+{/* <div class="launch">
+                <div class="inner-div-0">
+                    <img src="https://images2.imgbox.com/18/17/gCjLjHbl_o.png" alt="">
+                </div>
+                <div class="inner-div-1">
+                    <span>Falcon 9</span>
+                </div>
+                <div class="inner-div-2">
+                    <span>2010</span>
+                </div>
+            </div> */}
