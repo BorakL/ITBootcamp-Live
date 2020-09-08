4ee7e29504f4e459d6ac44413facb1a62516c371
peranovicc <cvijanperanovic@aol.com>
2020-09-08 11:53:44 +0200

    API React - useEffect

--- 8. Недеља/vezba/src/index.js
+++ 8. Недеља/vezba/src/index.js
@@ -5,11 +5,30 @@ import { v1 as uuidv1 } from 'uuid';
 
 // На клик параграфа за todo, променити стање done (и додати стилизацију)
 // Додати дугме delete које брише одређени todo
-const Todo = ({ todo }) => {
+const Todo = ({ todo,setTodos,todos }) => {
+
+  const deleteById = (id) => {
+    let index = todos.findIndex(el => el.id === id)
+    let tmp = [...todos]
+    tmp.splice(index,1)
+    setTodos(tmp)
+  }
+  const changeDone = (id) => {
+    let index = todos.findIndex(el => el.id === id)
+    let tmp = [...todos]
+    tmp[index].done = !tmp[index].done
+    setTodos(tmp)
+  }
+
   return (
-    <p>{todo.task} -- {todo.done + ''}</p>
+    <>
+      <p onClick={() => changeDone(todo.id)}>{todo.task} -- {todo.done + ''}</p>
+      <button onClick={() => deleteById(todo.id)}>X</button>
+    </>
   )
 }
+
+
 
 const App = () => {
   const [todos,setTodos] = useState([])
@@ -32,7 +51,7 @@ const App = () => {
       >
         Додај
       </button>
-      {todos.map(el => <Todo key={el.id} todo={el} />)}
+      {todos.map(el => <Todo key={el.id} todo={el} setTodos={setTodos} todos={todos} />)}
     </>
   )
 }
