0213b76eec413033547ed46c01343f5b425b145d
peranovicc <cvijanperanovic@aol.com>
2020-09-19 10:19:29 +0200

    Setup project

---
+++ 10. Недеља/students-app/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return (
+    <h1>Hello app</h1>
+  )
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
