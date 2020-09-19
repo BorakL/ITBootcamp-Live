e0b7d6015df1d97f866c3a39baab9ce4acd92a2d
peranovicc <cvijanperanovic@aol.com>
2020-09-19 11:48:27 +0200

    High level requests finished

--- 10. Недеља/students-app/src/services/studentsApi.js
+++ 10. Недеља/students-app/src/services/studentsApi.js
@@ -24,3 +24,18 @@ export const addStudent = (student) => axios.post(`${URL}/students`,student)
 export const addCourse = (course) => axios.post(`${URL}/courses`,course)
 export const addAssigment = (assigment) => axios.post(`${URL}/assigments`,assigment)
 export const addMaterial = (material) => axios.post(`${URL}/material`,material)
+
+// PATCH (UPDATE) REQUESTS
+export const updateLecturerById = (id,update) => axios.patch(`${URL}/lecturers/${id}`,update)
+export const updateStudentById = (id,update) => axios.patch(`${URL}/students/${id}`,update)
+export const updateCourseById = (id,update) => axios.patch(`${URL}/courses/${id}`,update)
+export const updateAssigmentById = (id,update) => axios.patch(`${URL}/assigments/${id}`,update)
+export const updateMaterialById = (id,update) => axios.patch(`${URL}/material/${id}`,update)
+
+// DELETE REQUESTS
+export const deleteLecturerById = (id) => axios.delete(`${URL}/lecturers/${id}`)
+export const deleteStudentById = (id) => axios.delete(`${URL}/students/${id}`)
+export const deleteCourseById = (id) => axios.delete(`${URL}/courses/${id}`)
+export const deleteAssigmentById = (id) => axios.delete(`${URL}/assigments/${id}`)
+export const deleteMaterialById = (id) => axios.delete(`${URL}/material/${id}`)
+
