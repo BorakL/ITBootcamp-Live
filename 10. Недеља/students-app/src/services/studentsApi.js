3c64c5b7b0f1c2bd03ba8be94b584a2becf516a4
peranovicc <cvijanperanovic@aol.com>
2020-09-19 10:53:22 +0200

    Post requests

--- 10. Недеља/students-app/src/services/studentsApi.js
+++ 10. Недеља/students-app/src/services/studentsApi.js
@@ -2,6 +2,7 @@ import axios from 'axios'
 
 const URL = 'http://localhost:3005'
 
+// GET REQUESTS
 export const getAllLecturers = () => axios.get(`${URL}/lecturers`)
 export const getAllStudents = () => axios.get(`${URL}/students`)
 export const getAllCourses = () => axios.get(`${URL}/courses`)
@@ -16,3 +17,10 @@ export const getAssigmentById = (id) => axios.get(`${URL}/assigment?id=${id}`)
 
 export const getLecturerById = (id) => axios.get(`${URL}/lecturers?id=${id}`)
 export const getLecturerByCourseId = (course_id) => axios.get(`${URL}/lecturers?courses_like${course_id}`)
+
+// POST REQUESTS
+export const addLecturer = (lecturer) => axios.post(`${URL}/lecturers`,lecturer)
+export const addStudent = (student) => axios.post(`${URL}/students`,student)
+export const addCourse = (course) => axios.post(`${URL}/courses`,course)
+export const addAssigment = (assigment) => axios.post(`${URL}/assigments`,assigment)
+export const addMaterial = (material) => axios.post(`${URL}/material`,material)
