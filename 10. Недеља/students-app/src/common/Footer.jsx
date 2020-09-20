a8086a2052f3269c8aa5d18a711d4cf4ae416105
peranovicc <cvijanperanovic@aol.com>
2020-09-21 00:01:03 +0200

    Структура фолдера, основне компоненте

---
+++ 10. Недеља/students-app/src/common/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <footer>
+            {/* Додатне информације */}
+            {/* Контакт подаци */}
+        </footer>
+    )
+}
+
+export default Footer
--- 10. Недеља/students-app/src/common/SideBar.jsx
+++ 10. Недеља/students-app/src/common/SideBar.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 const SideBar = () => {
     return (
         <aside>
-
+            {/* Logo */}
+            {/* Навигација */}
         </aside>
     )
 }
